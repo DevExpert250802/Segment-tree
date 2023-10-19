78941f42c329689ef81599af9ba0828e62bfdfd6
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-10-19 11:50:32 +0530

    Segment Tree for the Sum (Codeforces)

---
+++ Segment Tree for the Sum.cpp
@@ -0,0 +1,103 @@
+/*
+Input
+The first line contains two integers n and m (1 ≤ n , m ≤ 100000), the size of the array and the number of operations. The next line contains n
+ numbers ai, the initial state of the array (0 ≤ ai ≤10^9). The following lines contain the description of the operations.
+The description of each operation is as follows:
+
+1 i v: set the element with index i to v(0 ≤i <n, 0 ≤v ≤109).
+2 l r: calculate the sum of elements with indices from l to r−1 (0 ≤l <r ≤n).
+Output
+For each operation of the second type print the corresponding sum.
+Example
+input
+5 5
+5 4 2 3 5
+2 0 3
+1 1 1
+2 0 3
+1 3 1
+2 0 5
+output
+11
+8
+14
+         */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 2;
+int a[N], tree[4 * N];
+
+void build(int node, int start, int end) {
+    if (start == end) {
+        tree[node] = a[start];
+        return;
+    }
+    int mid = (start + end) / 2;
+    build(2 * node + 1, start, mid);
+    build(2 * node + 2, mid + 1, end);
+    tree[node] = tree[2 * node + 1]+ tree[2 * node + 2];
+}
+
+int query(int node, int start, int end, int l, int r) {
+    if (start > r || end < l) {
+        return 0;
+    }
+    if (l <= start && end <= r) {
+        return tree[node];
+    }
+    int mid = (start + end) / 2;
+    int q1 = query(2 * node + 1, start, mid, l, r);
+    int q2 = query(2 * node + 2, mid + 1, end, l, r);
+    return q1+ q2;
+}
+
+void update(int node, int start, int end, int ind, int val) {
+    if (start == end) {
+        a[start] = val;
+        tree[node] = val;
+        return;
+    }
+
+    int mid = (start + end) / 2;
+
+    if (ind <= mid) {
+        update(2 * node + 1, start, mid, ind, val);
+    } else {
+        update(2 * node + 2, mid + 1, end, ind, val);
+    }
+
+    tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    build(0, 0, n - 1);
+
+    while (m--) {
+        int type;
+        cin >> type;
+
+        if (type == 1) {
+            int ind, val;
+            cin >> ind >> val;
+            update(0, 0, n - 1, ind, val);
+        } else if (type == 2) {
+            int l, r;
+            cin >> l >> r;
+            int ans = query(0, 0, n - 1, l, r -1);
+            cout << ans << endl;
+        }
+    }
+    return 0;
+}
+
+
+// if you get error convert int to long long
+
