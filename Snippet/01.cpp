bccb5426fd9650e6bf1ab02cd45b2760cd7e680e
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-06-17 15:28:21 +0530

    Build a segment Tree

---
+++ Snippet/01.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/* Segment Tree Standard Code (Without Lazy) */
+class SegTree {
+public:
+    int N;Build 
+    vector<int> tree;
+
+    SegTree() {}
+    SegTree(int l) {
+        N = l;
+        tree.resize(4 * N);
+    }
+
+    // Build Segment Tree -- build(arr, 1, 0, N-1); 
+    void build(vector<int>& arr, int node, int start, int end) {
+        if (start == end) {
+            tree[node] = arr[start];
+            return;
+        }
+        int mid = (start + end) / 2;
+        build(arr, 2 * node, start, mid);
+        build(arr, 2 * node + 1, mid + 1, end);
+        tree[node] = tree[2 * node] + tree[2 * node + 1]; // Sum after backtracking
+    }
+};
+
+int main() {
+    // Shortcut to run the program is Ctrl + Shift + B
+    // Shortcut to run using separate test is Ctrl + Alt + B
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Create Object 
+    SegTree segTree(n);
+    segTree.build(arr, 1, 0, n - 1);    // Accessing member functions
+
+    // Build - View Build Data
+    for (int i = 1; i <  2*n; i++) {  // Printing only the relevant part of the segment tree
+        cout << segTree.tree[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
